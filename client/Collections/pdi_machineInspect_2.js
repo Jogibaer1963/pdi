c24fe4583e4e42e7260adf9d26a6955082e953a3
Jogibaer1963 <jogibaer99@gmail.com>
2017-06-22 17:20:59 -0500

    Fixing empty string

--- client/Collections/pdi_machineInspect_2.js
+++ client/Collections/pdi_machineInspect_2.js
@@ -179,7 +179,7 @@ if(Meteor.isClient) {
                             stringOrder = stringOrder.substring(stringEnd + posOrder);
                         }
             Meteor.call('machineUser', machineId, userLoggedIn, emailArray_2);
-            Meteor.call('sendEmail', ['juergen.hauser@claas.com', 'robert.schutte@claas.com'],
+            Meteor.call('sendEmail', ['juergen.hauser@claas.com'],
                   'Claas_Quality@mailgun.com', 'Parts Order request', userLoggedIn);
                      }
             }
