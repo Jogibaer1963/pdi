9a161d62e58e7abfb4ee3591becfda1b3a23eccf
Jogibaer1963 <jogibaer99@gmail.com>
2017-06-30 19:03:53 -0500

    fixes string in Fuel Consumption

--- client/Collections/pdi_fuelAfter.js
+++ client/Collections/pdi_fuelAfter.js
@@ -5,7 +5,7 @@ Template.fuelAfter.events({
         const selectedPdiMachine = Session.get('selectedPdiMachine');
         const fuelBeforePdi = Session.get('fuelBeforePdi');
         const fuelAfter = event.target.fuelAfterAll.value;
-        const consumption = (fuelAfter - fuelBeforePdi).toFixed(1);
+        const consumption = parseFloat((fuelAfter - fuelBeforePdi).toFixed(1));
         Meteor.call('fuelAfterPdi', selectedPdiMachine, fuelAfter, consumption);
         event.target.fuelAfterAll.value = '';
         Session.set('fuelBeforePdi', '');
